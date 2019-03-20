1ec4d35e0adb6e3ec148a08eb525d5c3b268f9f6
hanbin0306 <hanbin_goodluck@163.com>
2019-03-20 11:24:08 +0800

    GIT测试

--- erukaserver/src/main/java/com/forezp/eurekaserver/EurekaserverApplication.java
+++ erukaserver/src/main/java/com/forezp/eurekaserver/EurekaserverApplication.java
@@ -3,9 +3,11 @@ package com.forezp.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class EurekaserverApplication {
 
     public static void main(String[] args) {
