fbe0efaf85a9b63e50b2e6efa2f70def8abe001f
hanbin0306 <hanbin_goodluck@163.com>
2019-03-20 11:27:25 +0800

    GIT测试

--- erukaserver/src/main/java/com/forezp/eurekaserver/EurekaserverApplication.java
+++ erukaserver/src/main/java/com/forezp/eurekaserver/EurekaserverApplication.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
-@RestController
 public class EurekaserverApplication {
 
     public static void main(String[] args) {
